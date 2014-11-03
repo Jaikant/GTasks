993ee25b1589f65ff5fbe145719c51d981d470f6
jaikant <jaikant@gmail.com>
2014-11-04 00:51:42 +0530

    Refactored

---
+++ GTasks/GoogleAuth.swift
@@ -0,0 +1,9 @@
+//
+//  GoogleAuth.swift
+//  GTasks
+//
+//  Created by Jai on 02/11/14.
+//  Copyright (c) 2014 Jaikant Kumaran. All rights reserved.
+//
+
+import Foundation
---
+++ GTasks/UrgentTasksTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  UrgentTasksTableViewController.swift
+//  GTasks
+//
+//  Created by Jai on 23/10/14.
+//  Copyright (c) 2014 Jaikant Kumaran. All rights reserved.
+//
+
+import Foundation
